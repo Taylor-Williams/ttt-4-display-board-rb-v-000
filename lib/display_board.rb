15b9d6dadcd365ab703006f1bd7ce38306fe24cd
Taylor Williams <taylor@thewilliams.net>
2018-05-01 23:47:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,21 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(position)
+  counter = 0
+  def display_row(counter)
+    def display_board_space(state)
+      " #{state} "
+    end
+    puts "#{display_board_space(counter)}|#{display_board_space(counter+1)}|#{display_board_space(counter+2)}"
+  end
+  def display_row_divider
+    puts "-----------"
+  end
+  display_row(counter)
+  counter += 2
+  display_row_divider
+  display_row(counter)
+  counter += 2
+  display_row_divider
+  display_row
+end
